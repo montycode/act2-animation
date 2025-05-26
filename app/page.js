3ba58f0460af0c97f5830e96141a6ccc1bd5d2bb
cquezada123 <cquezada@humanfactor.mx>
2025-05-26 13:28:20 -0600

    file

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
       {/* Controles */}
       <div className="flex items-center justify-between w-3/4 mt-6 text-xl text-gray-400">
         <button>
-          <object type="image/svg+xml" data="boton1.svg"></object>
+          <object type="image/svg+xml" data={ArtistImg}></object>
         </button>
         <button>📤</button>
         <button>🔄</button>
